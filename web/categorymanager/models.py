660f6adac833d84e14b779138cd30aa5e7806f2c
SankojuRamesh <visitrameshs@gmail.com>
2024-09-26 14:39:27 +0530

    thumbnail

--- web/categorymanager/models.py
+++ web/categorymanager/models.py
@@ -32,9 +32,7 @@ class CategoryModel(models.Model):
         if img.mode == 'RGBA':
             background = Image.new("RGB", img.size, (255, 255, 255))  # White background
             background.paste(img, mask=img.split()[3])  # Paste image using transparency mask
-            img = background
-
-        # Save the image to BytesIO
+            img = background 
         thumb_io = BytesIO()
         img.save(thumb_io, format='JPEG')  # Save as JPEG format
 
@@ -50,4 +48,29 @@ class SubcategoryModel(models.Model):
     image_thumbnail = models.ImageField(upload_to='images/', default='')
     video_thumbnail = models.ImageField(upload_to='images/', default='')
     price =   models.CharField(max_length=100)
+
+    def save(self, *args, **kwargs): 
+        if self.thumbnail:
+            self.image_thumbnail = self.make_thumbnail(self.thumbnail)
+            super().save(*args, **kwargs)
+        
+    def make_thumbnail(self, image, size=(300, 300)):
+        img = Image.open(image)        
+        # Resize image maintaining the aspect ratio
+        img.thumbnail(size, Image.Resampling.LANCZOS)  # Use LANCZOS for high-quality resizing
+
+        # Convert RGBA to RGB if needed
+        if img.mode == 'RGBA':
+            background = Image.new("RGB", img.size, (255, 255, 255))  # White background
+            background.paste(img, mask=img.split()[3])  # Paste image using transparency mask
+            img = background
+
+        # Save the image to BytesIO
+        thumb_io = BytesIO()
+        img.save(thumb_io, format='JPEG')  # Save as JPEG format
+
+        # Return a Django File object
+        thumbnail = File(thumb_io, name=image.name.replace('.png', '.jpg'))
+        return thumbnail
+    
     
